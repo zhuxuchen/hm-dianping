2bb7b850e2696a3f59c73cf464ea3af31d76efbe
zhuxuchen <2517870679@qq.com>
2023-02-02 15:30:58 +0800

    乐观锁解决超卖

--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -83,8 +83,10 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         }
         // 5.扣减库存
         boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .setSql("stock = stock - 1")    // set stock = stock - 1
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)    // where voucher_id = ? and stock > 0
+                .update();
         if (!success) {
             // 扣减失败
             return Result.fail("库存不足！");
